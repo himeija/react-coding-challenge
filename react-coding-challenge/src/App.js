075770a1f0a323a3909652c9b2a7c69df81f5556
himeija <himeija@gmail.com>
2020-12-21 11:43:36 +0100

    add condition to read the list

--- react-coding-challenge/src/App.js
+++ react-coding-challenge/src/App.js
@@ -18,20 +18,28 @@ class App extends Component {
         console.log(json)
       })
       .then((result) =>{
-        this.setState([result])
+        this.setState({data: result})
       })
     }
   render() {
+
+    let listOfApplications
+
+    if (this.data === undefined) {
+      listOfApplications = 'loading';
+    }
+    else listOfApplications = this.state.data[0].id;
     
+
     return (
-      
+
       <div className="App">
         <header className="App-header">
           <p>Ici le titre de mon appli trop cool qui va charger et visualiser un fichier json</p>
         </header>
         <main>
           <p>voici ma liste : </p> 
-          <ul>{this.state.data.map}</ul>
+          <ul><li>{listOfApplications}</li></ul>
           <button onClick={() => console.log(`Bonjour !`)}>
             Trier
           </button>
